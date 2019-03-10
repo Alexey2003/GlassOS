de90f6ddd8962e91b45a6964411aae6137b1d074
alexey__ <alexey20032003@yandex.ru>
2019-03-10 17:08:48 +0300

    new logic

--- app.lua
+++ app.lua
@@ -1,5 +1,9 @@
 local gpu = require("component").gpu
+local filesystem = require("filesystem")
+local io = require("io")
 
-size = filesystem.size("./app/resourses/icon.pic")
-local file = filesystem.open("./app/resourses/icon.pic", "r")
-print(file.read(size))
+size = filesystem.size("~/app/resourses/icon.pic")
+local file = filesystem.open("~/app/resourses/icon.pic", "r")
+
+for i=1, i<10 do
+    print(file:read(1).."\n")
