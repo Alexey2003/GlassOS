cfe7506ba20bf096ec1484df8c380075fb62be3d
alexey__ <alexey20032003@yandex.ru>
2019-03-10 19:55:54 +0300

    fixed drawing bug

--- app.lua
+++ app.lua
@@ -2,7 +2,7 @@ local gpu = require("component").gpu
 local filesystem = require("filesystem")
 local io = require("io")
 
-local palette = { 0x000000, 0xFFFFFF, 0xFF0000, 0x00FF00, 0x0000FF }
+local palette = { 0x0F0F0F, 0xFFFFFF, 0xFF0000, 0x00FF00, 0x0000FF }
 
 size = filesystem.size("home/app/app/resourses/icon.pic")
 local file = filesystem.open("/home/app/app/resourses/icon.pic", "r")
@@ -33,7 +33,7 @@ y = tonumber(ys)
 for i=1, y do
   for j=1, x do
     gpu.setBackground(palette[tonumber(file:read(1))])
-    gpu.set(j, i, " ")
+    gpu.set((j)*2-1, i, "  ")
   end
 end
 
